3abc941c70df372045183ed5bcfd1724d65f2154
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-06-07 16:41:20 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.09 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        
+        // report 중복제거
+        HashSet<String> reportSet = new HashSet<String>();
+        for(String re : report){
+            reportSet.add(re);
+        }
+        
+        HashMap<String, Integer> repo = new HashMap<String, Integer>();
+        HashMap<String, Integer> mail = new HashMap<String, Integer>();
+        
+        for(int i = 0; i < id_list.length; i++){
+            repo.put(id_list[i], 0);
+            mail.put(id_list[i], i);
+        }
+                
+        for(String re : reportSet){
+            StringTokenizer st = new StringTokenizer(re);
+            String userId = st.nextToken();
+            String repoId = st.nextToken();
+            repo.put(repoId, repo.get(repoId)+1);
+        }
+        
+        for(String re : reportSet){
+            StringTokenizer st = new StringTokenizer(re);
+            String userId = st.nextToken();
+            String repoId = st.nextToken();
+            if(repo.get(repoId) >= k){
+                answer[mail.get(userId)]++;
+            }
+        }
+        
+
+        
+        return answer;
+    }
+}

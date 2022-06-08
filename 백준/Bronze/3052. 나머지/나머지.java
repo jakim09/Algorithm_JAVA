bc7e6aa4ae7da883882bcce3c3a036afe6133def
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-06-08 16:50:57 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -6,16 +6,10 @@ public class Main {
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int[] arr = new int[10];
-        
-        for(int i = 0; i < 10; i++){
-            arr[i] = Integer.parseInt(br.readLine()) % 42;
-        }
-        
         int[] result = new int[42];
         
         for(int i = 0; i < 10; i++){
-            result[arr[i]]++;
+            result[Integer.parseInt(br.readLine()) % 42]++;
         }
         int cnt = 0;
         for(int i = 0; i < 42; i++){

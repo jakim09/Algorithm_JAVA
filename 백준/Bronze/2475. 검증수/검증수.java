48ef31ce27e6fde3647fb3acca8dfbf3c1fa1ba1
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-06-08 13:32:45 +0900

    [Bronze V] Title: 검증수, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,19 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int sum = 0;
+        
+        while(st.hasMoreTokens()){
+            sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
+        }
+        
+        System.out.println(sum % 10);
+    }
+}

7af838c7e7c14467b52b2a454c50c4f1c3bea9a2
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-06-09 23:26:54 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -18,11 +18,15 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
                 
-        for (int i = 0; i < n; i++){
-            for (int j = i + 1; j < n; j++){
+        for (int i = 0; i < n - 2; i++){
+            for (int j = i + 1; j < n - 1; j++){
                 for (int k = j + 1; k < n; k++){
                     sum = arr[i]+arr[j]+arr[k];
-                    if(sum > max && sum <= m){
+                    if(sum == m){
+                        max = sum;
+                        break;
+                    }
+                    if(sum > max && sum < m){
                         max = sum;
                     }
                 }

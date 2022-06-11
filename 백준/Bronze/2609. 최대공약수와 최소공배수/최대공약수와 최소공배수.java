c47a85e639759872085b81f0b059bf71b5f48734
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-06-11 23:48:46 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -10,19 +10,25 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
-        for (int i = Math.min(n, m); i > 0; i--){
+        
+        // 최소공약수
+        int i;
+        for (i = Math.min(n, m); i > 0; i--){
             if(n % i == 0 && m % i == 0){
                 System.out.println(i);
                 break;
             }
         }
-        
+        // 최소공배수
+        System.out.println(n * m / i);        
+        /*
+        // 최소공배수
         for (int i = Math.max(n, m); i <= n * m; i++){
             if(i % n == 0 && i % m == 0){
                 System.out.println(i);
                 break;
             }
         }
-        
+        */
     }
 }

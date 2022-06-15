3e68f1bba6f077b8fbcc960850c0b4a3206cb359
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-06-15 21:38:34 +0900

    [Silver V] Title: 영화감독 숌, Time: 304 ms, Memory: 86416 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		int i = 666;
+		
+		while (cnt < N) {
+			if(String.valueOf(i++).contains("666")) {
+				cnt++;
+			}
+		}
+		System.out.println(i - 1);
+	}
+}

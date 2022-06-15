c3f50e555e539759ee06f93a1c9d6907cadf9d0c
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-06-15 19:14:29 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 132 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		while(t-- != 0) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			int[][] arr = new int[k+1][n+1];
+			
+			for(int i = 1; i <= n; i++) {
+				arr[0][i] = i;
+			}
+			
+			for (int i = 1; i <= k; i++) {
+				for (int j = 1; j <= n; j++) {
+					for (int a = 1; a <= j; a++) {
+					arr[i][j] += arr[i-1][a];
+					}
+				}
+			}
+			System.out.println(arr[k][n]);
+		}		
+	}
+}

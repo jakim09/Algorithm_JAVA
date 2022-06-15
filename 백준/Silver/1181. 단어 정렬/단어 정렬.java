509cf9b59a8e82dc6e889fdb0f1525d5033e677e
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-06-15 21:10:07 +0900

    [Silver V] Title: 단어 정렬, Time: 352 ms, Memory: 27960 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main { 
+	public static void main(String args[]) throws IOException {
+
+	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	 int n = Integer.parseInt(br.readLine());
+	 StringBuilder sb = new StringBuilder();
+	 String[] arr = new String[n];
+	 
+	 for(int i = 0; i < n; i++) {
+		 arr[i] = br.readLine();
+	 }
+	 
+	 Arrays.sort(arr, new Comparator<String>() {
+		public int compare(String s1, String s2) {
+			if(s1.length() == s2.length()){
+				return s1.compareTo(s2);
+			} else {
+				return s1.length() - s2.length();
+			}
+		}
+	 });
+	 
+	 sb.append(arr[0] + "\n");
+	 for(int i = 1; i < n; i++) {
+		 if(!arr[i].equals(arr[i-1])) {
+			 sb.append(arr[i] + "\n");
+		 }
+	 }
+	 System.out.println(sb);
+	}
+}

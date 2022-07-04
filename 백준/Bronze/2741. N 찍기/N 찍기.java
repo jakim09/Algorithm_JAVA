66ffeec93e6460573eb1ee6a6f1ac9c0725c51ce
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-07-04 14:31:59 +0900

    [Bronze V] Title: N 찍기, Time: 300 ms, Memory: 22728 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
         int n = Integer.parseInt(br.readLine());
         for(int i = 1; i <= n; i++){
-            System.out.println(i);
+            sb.append(i + "\n");
         }
+        System.out.println(sb);
+
     }
 }

4761e2ab871d8aca003461b8812bb9b32148f475
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-07-04 14:32:49 +0900

    [Bronze III] Title: 기찍 N, Time: 292 ms, Memory: 22152 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -3,13 +3,14 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main (String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        StringBuffer sb = new StringBuffer();
         int n = Integer.parseInt(br.readLine());
-        
-        for (int i = n; i > 0; i--){
-            System.out.println(i);
+        for(int i = n; i > 0; i--){
+            sb.append(i + "\n");
         }
+        System.out.println(sb);
+
     }
 }

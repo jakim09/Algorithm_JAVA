73ca74061f60ecb62830496b96e5964db0117bae
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-07-11 17:02:21 +0900

    [Silver III] Title: 파도반 수열, Time: 124 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -7,18 +7,27 @@ class Main {
 
     	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-       
-	    long[] dp = new long[101];
+        int[] arr = new int[t];
+        int max = -1;
+        for (int i = 0; i < t; i++) {
+        	arr[i] = Integer.parseInt(br.readLine());
+        	if(max < arr[i]) {
+        		max = arr[i];
+        	}
+        }
+        
+        int n = max;
+	    long[] dp = new long[n+1];
 	    dp[1] = 1;
 	    dp[2] = 1;
 	    dp[3] = 1;
-	    for (int i = 4; i < dp.length; i++) {
+	    for (int i = 4; i <= n; i++) {
 	    	dp[i] = dp[i-2] + dp[i-3];
 	    }
 	    
 	    StringBuffer sb = new StringBuffer();
 	    for(int i = 0; i < t; i++) {
-	    	sb.append(dp[Integer.parseInt(br.readLine())]).append("\n");
+	    	sb.append(dp[arr[i]]).append("\n");
 	    }
         System.out.println(sb);
     }

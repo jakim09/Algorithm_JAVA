af6737d8319862ab0449461317b14ad0f4e3760c
jakim09 <59862663+jakim09@users.noreply.github.com>
2022-07-21 15:49:49 +0900

    [Gold II] Title: 최대공약수 하나 빼기, Time: 576 ms, Memory: 130308 KB -BaekjoonHub

--- 백준/Gold/14476. 최대공약수 하나 빼기/최대공약수 하나 빼기.java
+++ 백준/Gold/14476. 최대공약수 하나 빼기/최대공약수 하나 빼기.java
@@ -29,13 +29,12 @@ public class Main {
 			int[] gcdResult = new int[N];
 			gcdResult[0] = arr2[1];
 			gcdResult[N-1] = arr1[N - 2];
-			int k = 0;
 			for (int i = 1; i < N - 1; i++) {
-				gcdResult[i] = gcd(arr1[k], arr2[k+2]);
-				k++;
+				gcdResult[i] = gcd(arr1[i-1], arr2[i+1]);
 			}
 			
 			int max = -1;
+			int k = 0;
 			for (int i = 0; i < N; i++) {
 				if(nums[i] % gcdResult[i] != 0) {
 					if (max < gcdResult[i]) {
@@ -51,13 +50,25 @@ public class Main {
 				System.out.println(max + " " + k);
 			}
 		}
-		
+
+		// 반복문
+		// gcd(a, b) == gcd(b, r) == gcd(b, a%b), stop when a%b==0
 		public static int gcd(int a, int b) {
-			int r = a % b;
-			if(r == 0) {
-				return b;
-			} else {
-				return gcd(b, r);
+			while(b != 0) {
+				int r = a % b;
+				a = b;
+				b = r;
 			}
-		}	
+			return a;
+		}
+
+		// 재귀 (생각 안나면 써도 되나 확장 유클리드 호제법에서 사용하기 어려움)
+//		public static int gcd(int a, int b) {
+//			int r = a % b;
+//			if(r == 0) {
+//				return b;
+//			} else {
+//				return gcd(b, r);
+//			}
+//		}	
 }
